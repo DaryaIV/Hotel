a94bcf2fd356c37ff31ef58976efbd4ba3936ba0
DaryaIV <ilina.darya.92@gmail.com>
2020-04-27 18:35:15 +0700

    Компонент поле ввода

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -1,2 +1,4 @@
-import '../components/layout/layout.scss';
-import './form-elements/buttons/button.scss';
+import "../components/layout/layout.scss";
+import "./form-elements/buttons/button.scss";
+import "./form-elements/text-field/text-field.scss";
+import 'ui.scss';
