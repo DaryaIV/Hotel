211b46e626c17573d450b711d0c9cef663768ec3
DaryaIV <ilina.darya.92@gmail.com>
2020-04-29 16:08:20 +0700

    Компонент радио кнопка

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -1,4 +1,5 @@
 import "../components/layout/layout.scss";
 import "./form-elements/buttons/button.scss";
 import "./form-elements/text-field/text-field.scss";
-import 'ui.scss';
+import "./form-elements/radio-button/radio-button.scss";
+import './ui.scss';
