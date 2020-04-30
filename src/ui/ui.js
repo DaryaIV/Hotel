8920fc584e83627c0410d1d69a727ebad7cfa3dd
DaryaIV <ilina.darya.92@gmail.com>
2020-04-30 18:35:28 +0700

    Компонент флажок и переключатель

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -2,4 +2,6 @@ import "../components/layout/layout.scss";
 import "./form-elements/buttons/button.scss";
 import "./form-elements/text-field/text-field.scss";
 import "./form-elements/radio-button/radio-button.scss";
+import "./form-elements/checkbox/checkbox.scss";
+import "./form-elements/toggle/toggle.scss";
 import './ui.scss';
