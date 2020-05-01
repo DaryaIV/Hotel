0d9639ac4de4ff8ac2cbb8e3ea82ecdd1dfc4e93
DaryaIV <ilina.darya.92@gmail.com>
2020-05-01 18:14:56 +0700

    Компонент like button

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -4,4 +4,5 @@ import "./form-elements/text-field/text-field.scss";
 import "./form-elements/radio-button/radio-button.scss";
 import "./form-elements/checkbox/checkbox.scss";
 import "./form-elements/toggle/toggle.scss";
+import "./form-elements/like-btn/like-btn.scss";
 import './ui.scss';
